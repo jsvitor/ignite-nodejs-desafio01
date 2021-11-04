66345ddce662415c5fdbb39d81d9b292b8bd6c06
Vitor <jszvitor@gmail.com>
2021-11-04 10:12:34 -0300

    feat(main): added user creation and verification middleware

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body;
+
+  const userAlreadyExists = users.find(user => user.username == username)
+  // IF user not exists, return error message
+  if (!userAlreadyExists) {
+    return response.status(400).json({ "message": "User not exists" })
+  }
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  // Desestruturar o request.body
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.find(user => user.username == username)
+  
+  if (userAlreadyExists) {
+    return response.status(400).json({ "message": "Username already exists" })
+  }
+
+  users.push({
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  });
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
