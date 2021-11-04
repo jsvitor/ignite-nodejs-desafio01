e669b1d35ef5cbae76735662c31ea0580e7a5129
Vitor <jszvitor@gmail.com>
2021-11-04 14:46:01 -0300

    feat(main): added create todo rote

--- src/index.js
+++ src/index.js
@@ -11,15 +11,16 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.body;
+  const { username } = request.headers;
 
   const userAlreadyExists = users.find(user => user.username == username)
+  
   // IF user not exists, return error message
   if (!userAlreadyExists) {
     return response.status(400).json({ "message": "User not exists" })
   }
-
-  return next()
+  request.user = userAlreadyExists;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -43,11 +44,25 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // Lista Todos
+  return response.status(201).json(request.user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // Cria Todos
+  const { title, deadline } = request.body;
+
+  const task = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  request.user.todos.push(task)
+
+  return response.status(201).json(task)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
